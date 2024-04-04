921ee010376db513fd12779078e6e856a5537bd3
rmtrescato <rmenezestrescato@gmail.com>
2024-04-04 09:22:30 -0300

    inclusao de dados_json

--- app.py
+++ app.py
@@ -10,10 +10,18 @@ if response.status_code == 200:
     dados_restaurante = {}
     for item in dados_json:
         nome_do_restaurante = item['Company']
-        
-    
-    
+        if nome_do_restaurante not in dados_restaurante: 
+            dados_restaurante[nome_do_restaurante] = []
+            
+        dados_restaurante[nome_do_restaurante].append({
+            'item': item['Item'],
+            'price': item['price'],
+            'description': item['description']
+        })
+ 
     
     
 else:
     print(f'O erro foi {response.status_code}')
+
+print(dados_restaurante['McDonald’s'])
